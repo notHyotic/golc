460ec1c7fe58498373d21d04faabaf7dc0d0449a
notHyotic <hy0t1c1@gmail.com>
2025-06-07 14:17:49 -0500

    lc 62

---
+++ solutions/neetcode150/14.2D-DynamicProgramming/unique_path.go
@@ -0,0 +1,24 @@
+package dp2d
+
+// Leetcode #62
+func uniquePaths(m int, n int) int {
+    row := make([]int, n)
+	for i := 0; i < n; i++ {
+		row[i] = 1
+	}
+
+	for i := 0; i < m-1; i++ {
+		newRow := make([]int, n)
+		for j := 0; j < n; j++ {
+			newRow[j] = 1
+		}
+
+		for j := n-2; j > -1; j-- {
+			newRow[j] = newRow[j+1] + row[j]
+		}
+
+		row = newRow
+	}
+
+	return row[0]
+}
